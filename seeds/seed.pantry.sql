17cd79d4e89d4cd60e14af7812c21549d59fb821
Catherine Davis <catherinevtdavis@gmail.com>
2020-11-04 00:47:53 -0700

    added seed files for pantry and shopping list

---
+++ seeds/seed.pantry.sql
@@ -0,0 +1,17 @@
+INSERT INTO pantry (item_name, user_id, category_1, category_2, category_3, category_4, category_5, category_6, category_7, quantity)
+VALUES
+    ('Chicken Sandwich', 1, 'protein', null, null, null, null, null, null, 1),
+    ('Lunchable: Turkey Sandwich', 1, 'protein', 'carb', 'combo', null, null, null, null, 3),
+    ('Spaghetti with Meatballs', 1, 'protein', 'carb', 'combo', null, null, null, null, 4),
+    ('Mashed Potatoes', 1, 'carb', null, null, null, null, null, null, 1),
+    ('Corn', 1, 'vegetable', null, null, null, null, null, null, 5),
+    ('Peas', 1, 'vegetable', null, null, null, null, null, null, 6),
+    ('Green Beans', 1, 'vegetable', null, null, null, null, null, null, 3),
+    ('Apple Slices', 1, 'fruit', null, null, null, null, null, null, 2),
+    ('Grapes', 1, 'fruit', null, null, null, null, null, null, 4),
+    ('Watermelon', 1, 'fruit', null, null, null, null, null, null, 3),
+    ('Milk', 1, 'drink', null, null, null, null, null, null, 1),
+    ('Chocolate Milk', 1, 'drink', null, null, null, null, null, null, 3),
+    ('Juice', 1, 'drink', null, null, null, null, null, null, 2),
+    ('Brownie', 1, 'dessert', null, null, null, null, null, null, 3),
+    ('Cookie', 1, 'dessert', null, null, null, null, null, null, 2);
---
+++ seeds/seed.shopping_list.sql
@@ -0,0 +1,8 @@
+INSERT INTO shopping_list (user_id, title, items)
+VALUES
+    (1, 'Shopping List 1', ARRAY ['Chicken Sandwich', 'Milk', 'Corn']),
+    (1, 'Shopping List 2', ARRAY ['Spaghetti and Meatballs', 'Chocolate Milk', 'Cookie']),
+    (1, 'Shopping List 3', ARRAY ['Watermelon', 'Juice', 'Cookie']),
+    (2, 'Shopping List 1', ARRAY ['Lunchable: turkey sandwich', 'Grapes', 'Milk']),
+    (3, 'Shopping List 1', ARRAY ['Chicken Soup', 'Cottage Cheese', 'Grapes']),
+    (3, 'Shopping List 2', ARRAY ['Dog food', 'Dog food', 'Water']);
