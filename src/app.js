dc7fb350e85d525be1e07ef5f90e7594824e4cd4
Kevin Davis <Kevindavis5454@gmail.com>
2020-11-14 08:32:55 -0700

    added cors whitelists

--- src/app.js
+++ src/app.js
@@ -1,44 +1,57 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
 
-const usersRouter = require('./users/users-router')
-const itemsRouter = require('./items/items-router')
-const authRouter = require('./auth/auth-router')
-const PantryRouter = require('./pantry/pantry-router')
-const SavedLunchRouter = require('./saved_lunches/saved_lunches-router')
+const usersRouter = require("./users/users-router");
+const itemsRouter = require("./items/items-router");
+const authRouter = require("./auth/auth-router");
+const PantryRouter = require("./pantry/pantry-router");
+const SavedLunchRouter = require("./saved_lunches/saved_lunches-router");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+const whitelist = [
+  "https://tiny-lunches-app.vercel.app",
+  "http://localhost:3000",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || origin === undefined) {
+      callback(null, true);
+    } else {
+      console.log({ origin });
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
-app.use('/api/users', usersRouter)
-app.use('/api/items', itemsRouter)
-app.use('/api/auth', authRouter)
-app.use('/api/pantry', PantryRouter)
-app.use('/api/savedlunches', SavedLunchRouter)
+app.use("/api/users", usersRouter);
+app.use("/api/items", itemsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/pantry", PantryRouter);
+app.use("/api/savedlunches", SavedLunchRouter);
 
-app.get('/', (req,res) => {
-    res.send('Hello, world!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
 
 app.use(function errorHandler(error, req, res, next) {
-       let response
-       if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
-module.exports = app
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+module.exports = app;
