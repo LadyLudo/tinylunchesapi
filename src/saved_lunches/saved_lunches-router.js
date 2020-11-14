6ea52d146e6db840b6ed95daf221c0036b175314
Kevin Davis <Kevindavis5454@gmail.com>
2020-11-14 13:49:23 -0700

    changed data type returned for SavedLunches

--- src/saved_lunches/saved_lunches-router.js
+++ src/saved_lunches/saved_lunches-router.js
@@ -1,124 +1,100 @@
-const express = require('express')
-const SavedLunchService = require('./saved_lunches-service')
-const { requireAuth } = require('../middleware/jwt-auth')
+const express = require("express");
+const SavedLunchService = require("./saved_lunches-service");
+const { requireAuth } = require("../middleware/jwt-auth");
 
-const SavedLunchRouter = express.Router()
-const jsonParser = express.json()
+const SavedLunchRouter = express.Router();
+const jsonParser = express.json();
 
-SavedLunchRouter
-    .route('/')
-    .all(requireAuth)
-    .get((req, res, next) => {
-        SavedLunchService.getAllLists(
-            req.app.get('db')
-        )
-            .then(lists => {
-                res.json(lists)
-            })
-            .catch(next)
-    })
-    .post(jsonParser, (req, res, next) => {
-        const { user_id, title, items } = req.body
-        const newList = { user_id, title, items }
+SavedLunchRouter.route("/")
+  .all(requireAuth)
+  .get((req, res, next) => {
+    SavedLunchService.getAllLists(req.app.get("db"))
+      .then((lists) => {
+        res.json([lists]);
+      })
+      .catch(next);
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { user_id, title, items } = req.body;
+    const newList = { user_id, title, items };
 
-        
-        for (const [key, value] of Object.entries(newList)) {
-            if (value == null) {
-                return res.status(400).json({
-                    error: { message: `Missing '${key}' in request body` }
-                })
-            }
+    for (const [key, value] of Object.entries(newList)) {
+      if (value == null) {
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` },
+        });
+      }
+    }
+
+    SavedLunchService.insertList(req.app.get("db"), newList)
+      .then((list) => {
+        res.status(201).json(list);
+      })
+      .catch(next);
+  });
+
+SavedLunchRouter.route("/:id")
+  .all(requireAuth)
+  .all((req, res, next) => {
+    SavedLunchService.getById(req.app.get("db"), req.params.id)
+      .then((list) => {
+        if (!list) {
+          return res.status(404).json({
+            error: { message: `List doesn't exist` },
+          });
         }
+        res.list = list;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json(res.list);
+  })
+  .delete((req, res, next) => {
+    SavedLunchService.deleteList(req.app.get("db"), req.params.id)
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { user_id, title, items } = req.body;
+    const listToUpdate = { user_id, title, items };
 
-        SavedLunchService.insertList(
-            req.app.get('db'),
-            newList
-        )
-            .then(list => {
-                res
-                    .status(201)
-                    .json(list)
-            })
-            .catch(next)
-    })
+    const numberOfValues = Object.values(listToUpdate).filter(Boolean).length;
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain at least one value`,
+        },
+      });
+    }
 
-SavedLunchRouter
-    .route('/:id')
-    .all(requireAuth)
-    .all((req, res, next) => {
-        SavedLunchService.getById(
-            req.app.get('db'),
-            req.params.id
-        )
-            .then(list => {
-                if(!list) {
-                    return res.status(404).json({
-                        error: { message: `List doesn't exist`}
-                    })
-                }
-                res.list = list
-                next()
-            })
-            .catch(next)
-    })
-    .get((req, res, next) => {
-        res.json(res.list)
-    })
-    .delete((req, res, next) => {
-        SavedLunchService.deleteList(
-            req.app.get('db'),
-            req.params.id
-        )
-            .then(() => {
-                res.status(204).end()
-            })
-            .catch(next)
-    })
-    .patch(jsonParser, (req, res, next) => {
-        const { user_id, title, items } = req.body
-        const listToUpdate = { user_id, title, items }
+    SavedLunchService.updateList(req.app.get("db"), req.params.id, listToUpdate)
+      .then((result) => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
 
-        const numberOfValues = Object.values(listToUpdate).filter(Boolean).length
-        if (numberOfValues === 0) {
-            return res.status(400).json({
-                error: {
-                    message: `Request body must contain at least one value`
-                }
-            })
+SavedLunchRouter.route("/users/:user_id")
+  .all(requireAuth)
+  .all((req, res, next) => {
+    SavedLunchService.getByUserId(req.app.get("db"), req.params.user_id)
+      .then((list) => {
+        if (!list[0]) {
+          return res.status(404).json({
+            error: { message: `List doesn't exist` },
+          });
         }
+        res.list = list;
+        next();
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.send(res.list);
+  });
 
-        SavedLunchService.updateList(
-            req.app.get('db'),
-            req.params.id,
-            listToUpdate
-        )
-            .then(result => {
-                res.status(204).end()
-            })
-            .catch(next)
-    })
-
-SavedLunchRouter
-    .route('/users/:user_id')
-    .all(requireAuth)
-    .all((req,res,next) => {
-        SavedLunchService.getByUserId(
-            req.app.get('db'),
-            req.params.user_id
-        )
-            .then(list => {
-                if(!list[0]) {
-                    return res.status(404).json({
-                        error: { message: `List doesn't exist` }
-                    })
-                }
-                res.list = list
-                next()
-            })
-            .catch(next)
-    })
-    .get((req, res, next) => {
-        res.send(res.list)
-    })
-
-module.exports = SavedLunchRouter
+module.exports = SavedLunchRouter;
