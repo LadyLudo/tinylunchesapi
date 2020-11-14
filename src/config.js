b3479c812cd9ab361c370f7df5d6190d0e3746b8
Kevin Davis <Kevindavis5454@gmail.com>
2020-11-14 10:46:14 -0700

    troubleshooting JWT

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://catherinedavis@localhost/tinylunches',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://catherinedavis@localhost/tinylunchestest',
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '20s',
-  }
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://catherinedavis@localhost/tinylunches",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://catherinedavis@localhost/tinylunchestest",
+  JWT_SECRET: process.env.JWT_SECRET || "tiny-lunch-jwt",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "20s",
+};
