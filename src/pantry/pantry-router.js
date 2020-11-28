5788f95b4c3f491a78269c821e95efb24ce71a88
Kevin Davis <Kevindavis5454@gmail.com>
2020-11-28 13:51:42 -0700

    fixed pantry return

--- src/pantry/pantry-router.js
+++ src/pantry/pantry-router.js
@@ -141,9 +141,10 @@ PantryRouter.route("/users/:user_id")
     PantryService.getByUserId(req.app.get("db"), req.params.user_id)
       .then((item) => {
         if (!item[0]) {
-          return res.status(404).json({
-            error: { message: `No Pantry Items exist for this User` },
-          });
+          return [];
+          //   return res.status(404).json({
+          //     error: { message: `No Pantry Items exist for this User` },
+          //   });
         }
         res.item = item;
         next();
